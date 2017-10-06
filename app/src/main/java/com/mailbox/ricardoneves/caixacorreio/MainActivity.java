28964b95675557958de9f0b2fa8c1323a64dafce
Ricardo  Neves <ricardoneves@Ricardos-MacBook-Pro-2.local>
2017-10-06 22:16:22 +0100

    Added volley dependecy tp get the mail and door status

--- app/src/main/java/com/mailbox/ricardoneves/caixacorreio/MainActivity.java
+++ app/src/main/java/com/mailbox/ricardoneves/caixacorreio/MainActivity.java
@@ -2,14 +2,17 @@ package com.mailbox.ricardoneves.caixacorreio;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
 import android.media.MediaPlayer;
-import android.support.v4.content.LocalBroadcastManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -17,24 +20,41 @@ import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-import com.google.firebase.iid.FirebaseInstanceId;
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.messaging.FirebaseMessaging;
 
-import java.util.Objects;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final static String NEW_MAIL_MSG = "new_mail";
-    private final static String OPEN_DOOR_MSG = "open_door";
-    private final static String CLOSE_DOOR_MSG = "close_door";
-    private final static String RESET_MSG = "reset_mail";
+    private static final String NEW_MAIL_MSG = "new_mail";
+    private static final String OPEN_DOOR_MSG = "open_door";
+    private static final String CLOSE_DOOR_MSG = "close_door";
+    private static final String RESET_MSG = "reset_mail";
 
-    private final static String RED_COLOR = "#FF0000";
-    private final static String GREEN_COLOR = "#00FF00";
+    private static final String RED_COLOR = "#FF0000";
+    private static final String GREEN_COLOR = "#00FF00";
+
+    private static final String URL_STRING = "http://ricardoneves.noip.me:8090/current_state";
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "password";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getState(getApplicationContext());
 
         // Register receiver to receive Notifications from firebaseMessagingService
         LocalBroadcastManager.getInstance(this).registerReceiver((mMessageReceiver), new IntentFilter("mail_data"));
@@ -68,34 +88,117 @@ public class MainActivity extends AppCompatActivity {
         public void onReceive(Context context, Intent intent) {
             String messageBody = intent.getExtras().getString("body");
             if(!TextUtils.isEmpty(messageBody)) {
-                LinearLayout mail_status_layout = (LinearLayout) findViewById(R.id.mail_status_layout);
-                LinearLayout door_status_layout = (LinearLayout) findViewById(R.id.door_status_layout);
-                ImageView mail_status_img = (ImageView) findViewById(R.id.mail_status_img);
-                ImageView door_status_img = (ImageView) findViewById(R.id.door_status_img);
                 switch (messageBody) {
                     case NEW_MAIL_MSG:
-                        mail_status_layout.setBackgroundColor(Color.parseColor(GREEN_COLOR));
-                        mail_status_img.setImageResource(R.drawable.open_email);
-                        MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.door_bell);
-                        mp.start();
+                        newMail();
                         break;
                     case OPEN_DOOR_MSG:
-                        door_status_layout.setBackgroundColor(Color.parseColor(RED_COLOR));
-                        door_status_img.setImageResource(R.drawable.open_door);
+                        openDoor();
                         break;
                     case CLOSE_DOOR_MSG:
-                        door_status_layout.setBackgroundColor(Color.parseColor(GREEN_COLOR));
-                        door_status_img.setImageResource(R.drawable.close_door);
+                        closeDoor();
                         break;
                     case RESET_MSG:
-                        mail_status_layout.setBackgroundColor(Color.parseColor(RED_COLOR));
-                        mail_status_img.setImageResource(R.drawable.close_email);
+                        resetMail();
                         break;
                 }
             }
-
-
-
         }
     };
+
+    private void newMail() {
+        LinearLayout mail_status_layout = (LinearLayout) findViewById(R.id.mail_status_layout);
+        ImageView mail_status_img = (ImageView) findViewById(R.id.mail_status_img);
+
+        mail_status_layout.setBackgroundColor(Color.parseColor(GREEN_COLOR));
+        mail_status_img.setImageResource(R.drawable.open_email);
+        MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.door_bell);
+        mp.start();
+    }
+
+    private void openDoor() {
+        LinearLayout door_status_layout = (LinearLayout) findViewById(R.id.door_status_layout);
+        ImageView door_status_img = (ImageView) findViewById(R.id.door_status_img);
+
+        door_status_layout.setBackgroundColor(Color.parseColor(RED_COLOR));
+        door_status_img.setImageResource(R.drawable.open_door);
+    }
+
+    private void closeDoor() {
+        LinearLayout door_status_layout = (LinearLayout) findViewById(R.id.door_status_layout);
+        ImageView door_status_img = (ImageView) findViewById(R.id.door_status_img);
+
+        door_status_layout.setBackgroundColor(Color.parseColor(GREEN_COLOR));
+        door_status_img.setImageResource(R.drawable.close_door);
+    }
+
+    private void resetMail() {
+        LinearLayout mail_status_layout = (LinearLayout) findViewById(R.id.mail_status_layout);
+        ImageView mail_status_img = (ImageView) findViewById(R.id.mail_status_img);
+
+        mail_status_layout.setBackgroundColor(Color.parseColor(RED_COLOR));
+        mail_status_img.setImageResource(R.drawable.close_email);
+    }
+
+    private void showAppDialog(String title, String message) {
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(message);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+    }
+
+
+    private void getState(final Context context) {
+        String baseAuthCredentials = USERNAME + ':' + PASSWORD;
+        final String encodedCredentials = Base64.encodeToString(baseAuthCredentials.getBytes(), Base64.NO_WRAP);
+
+        RequestQueue queue = Volley.newRequestQueue(context);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL_STRING, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("response", response.toString());
+                        try {
+                            String doorState = response.get("door").toString();
+                            String mailState = response.get("mail").toString();
+
+                            if("opened".equals(doorState))
+                                openDoor();
+                            if("has_mail".equals(mailState))
+                                newMail();
+
+
+                        } catch (JSONException e) {
+                        }
+                    }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("response", error.toString());
+                        showAppDialog("Erro", "Não foi possível ir buscar o estado");
+                    }
+                }
+        ){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Basic " + encodedCredentials);
+                return headers;
+            }
+        };
+
+        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy());
+        // Make the call for the HTTP request
+        queue.add(jsonObjectRequest);
+    }
+
+
 }
